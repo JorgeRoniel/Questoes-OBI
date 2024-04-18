d8707472588230ac29c4c53a24f9227b00927558
JorgeRoniel <jorge.infor4g@gmail.com>
2024-04-18 19:01:36 -0300

    Arquivos.py corrigido

--- Fase1/2015/nivel1/arquivos.py
+++ Fase1/2015/nivel1/arquivos.py
@@ -1,13 +1,21 @@
 #Aqui, pegamos o numero de arquivos e o numero maximo de pastas, conforme pede a questao
 N, B = [int(n) for n in input().split()]
 
-tamanhos = [int(i) for i in input('', end='').split() if int(i) <= B] #uma lista para cada arquivo
-
+tamanhos = [int(i) for i in input().split() if int(i) <= B] #uma lista para cada arquivo
+tamanhos.sort()
 soma = 0 #uma variavel soma para dividir mais tarde com o tamanho maximo da pasta
 
 #um for para somar os tamanhos de cada pasta
-for tamanho in tamanhos:
-    soma = soma + tamanho
+i = 0 
+j = len(tamanhos) - 1
+while j > i:
+    if tamanhos[i] + tamanhos[j] <= B:
+        i += 1
+    soma += 1
+    j -= 1
 
-totalPasta = soma/B #dividimos o valor da soma de cada pasta com o tamanho maximo das pastas
-print(int(totalPasta)) #mostramos na tela o resultado final
+if i == j:
+    soma += 1    
+
+#dividimos o valor da soma de cada pasta com o tamanho maximo das pastas
+print(soma)#mostramos na tela o resultado final
