d6691cc66992dd79e3b2080bc3a93af67716fd98
JorgeRoniel <jorge.infor4g@gmail.com>
2024-04-24 09:47:02 -0300

    Correções

--- Fase1/2015/nivel2/quebra_cabeca.py
+++ Fase1/2015/nivel2/quebra_cabeca.py
@@ -1,12 +1,12 @@
 MAX = 200000 #numero maximo estabelecido pela questão
-N = int(input("Numero de peças: ")) #pegamos do usuario o numero de peças a ser usado
+N = int(input()) #pegamos do usuario o numero de peças a ser usado
 
 #Inicializamos a lista de letras e a listas dos numeros que estão a direita
 letra = [0 for i in range(MAX+1)] 
 direita = [0 for i in range(MAX+1)]
 
 for i in range(N):
-    I = input('Insira a peça: ').split() #Aqui recebemos as peças inseridas pelo usuario, a função split divide a entrada e guarda no I como uma lista
+    I = input().split() #Aqui recebemos as peças inseridas pelo usuario, a função split divide a entrada e guarda no I como uma lista
     E = int(I[0]) #O 'E' representa o numero que fica a esquerda, então pegamos o valor que esta na posição 0 do I
     letra[E] = I[1] #A lista letra na posição 'E', que é o numero que fica a esquerda, recebe o valor que esta na posição 1 de 'I'
     direita[E] = int(I[2]) #A lista direita recebe o elemento que esta na posição 2 de 'I'
