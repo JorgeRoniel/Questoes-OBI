a5be4e764e619d4fddb6c2de1440621bc592099e
JorgeRoniel <jorge.infor4g@gmail.com>
2024-04-25 18:37:21 -0300

    Questão Pacman corrigida

--- Fase1/2014/nivel2/pacman.py
+++ Fase1/2014/nivel2/pacman.py
@@ -2,27 +2,37 @@
 tab = int(input())
 
 jogadas = [] #definimos uma lista para guardar o conjunto de jogadas
-jog = [] #definimos outra lista para guardar cada jogada
-
+jog = []
 #aqui o usuario vai inserir a jogada, que vai ser guardada em 'jog', apos isso é guardada em jogadas
 for i in range(tab):
-    jog = input().split()
+    j = input()
+    jog = list(j)
     jogadas.append(jog)
 
 cont = 0 #definimos um contador, para contar quantos 'o' irão aparecer
+l = [] #definimos essa lista para pegar os valores da variavel cont e depois pegar o maior
 
-for jogada in jogadas: #acesso cada jogada em jogadas
-    for letra in jogada: #acesso o conteudo de cada jogada
+for i in range(len(jogadas)): #o primeiro for itera sobre as linhas da tabela
+    if i % 2 == 0: #se o indice da linha for par, o segundo for ira percorrer as colunas da esq -> dir
+        for j in range(len(jogadas)):
+            if jogadas[i][j] == 'o': #se ele encontrar um 'o', então cont aumenta 1
+                cont +=1
+                if jogadas[i][j] == 'A': #Caso, depois dele achar um 'o', e depois vinher um A, ele adiciona cont na lista e depois zera ele
+                    l.append(cont)
+                    cont = 0
+            elif jogadas[i][j] == 'A': #caso o elemento seja o 'A', então já faz o mesmo processo acima
+                    l.append(cont)
+                    cont = 0
+    else:
+        for j in range(len(jogadas)-1, 0-1, -1):# caso o indice da linha seja impar, ela é percorrida da dir -> esq
+            if jogadas[i][j] == 'o':
+                cont +=1
+                if jogadas[i][j] == 'A':
+                    l.append(cont)
+                    cont = 0
+            elif jogadas[i][j] == 'A':
+                    l.append(cont)
+                    cont = 0
+l.append(cont) #add cont na lista
 
-        #Aqui, verifico se na linha contem um "A", pois quando a linha tem um "A", ela não vale para 
-        #a contagem.
-        if 'A' in letra:
-            cont = cont
-        
-        #caso não tenha o "A", vamos verificar cada elemento, e toda vez que tiver o "o", cont aumenta mais 1.
-        else:
-            for i in letra:
-                if i == 'o':
-                    cont += 1
-
-print(cont) #mostra na tela o numero de "o", como pedido na questão.
+print(max(l))
