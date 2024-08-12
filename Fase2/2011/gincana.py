c35deb8853f190ced4614fdba9c413e4d8dc5318
JorgeRoniel <jorge.infor4g@gmail.com>
2024-08-12 13:27:46 -0300

    Fase 2 - 2013

--- Fase2/2011/gincana.py
+++ Fase2/2011/gincana.py
@@ -1,3 +1,12 @@
+'''
+Se o nó atual (atual) ainda não foi visitado (visitados[atual] == False):
+Marca o nó como visitado (visitados[atual] = True).
+
+O código então percorre todos os vizinhos do nó atual (for vizinho in adjacencias[atual]:).
+Se um vizinho ainda não foi visitado (visitados[vizinho] == False), a função dfs é chamada 
+recursivamente para explorar esse vizinho.
+'''
+
 def dfs(adjacencias, no, visitados):
     atual = no
     if visitados[atual] == False:
@@ -12,7 +21,17 @@ for x in range(m):
     alunos = [int(num) for num in input().split()]
     l.append(alunos)
 
+'''
+adjacencias: Um dicionário que será usado para armazenar o grafo,
+onde cada estudante é uma chave e seu valor é uma lista de estudantes 
+conectados.
+'''
 adjacencias = {i: [] for i in range(1, n+1)}
+
+'''
+visitados: Um dicionário que marca se cada 
+estudante foi visitado (False inicialmente para todos).
+'''
 visitados = {i: False for i in range(1, n+1)}
 
 for i, j in l:
@@ -25,4 +44,4 @@ for estudantes in range(1, n+1):
           dfs(adjacencias, estudantes, visitados)
           num_times += 1
 
-print(num_times)
+print(num_times) #mostra quantos times é possivel ser montados.
---
+++ Fase2/2013/troia.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+'''
+A função bfs é usada para explorar 
+todos os vértices que pertencem ao mesmo componente conexo, 
+começando de um vértice inicial "start".
+'''
+
+def bfs(start, visitado, adj):
+    fila = deque([start])
+    visitado[start] = True
+    
+    while fila:
+        node = fila.popleft()
+        for vizinho in adj[node]:
+            if not visitado[vizinho]:
+                visitado[vizinho] = True
+                fila.append(vizinho)
+
+'''
+Para cada vértice não visitado, inicia-se uma nova BFS, o que indica que 
+encontramos uma nova família. O contador familias é incrementado para cada 
+componente conexo encontrado.
+'''
+
+def contar_familias(N, relacionamentos):
+    # Criar a lista de adjacência
+
+    '''
+    adj é uma lista de listas, 
+    onde adj[i] contém todos os vértices que 
+    estão conectados ao vértice i.
+    '''
+    
+    adj = [[] for _ in range(N+1)]
+    
+    for a, b in relacionamentos:
+        adj[a].append(b)
+        adj[b].append(a)
+    
+    visitado = [False] * (N + 1)
+    familias = 0
+    
+    # Realizar BFS para encontrar todos os componentes conexos
+    for i in range(1, N + 1):
+        if not visitado[i]:
+            bfs(i, visitado, adj)
+            familias += 1
+    
+    return familias
+
+# Leitura dos dados de entrada
+N, M = map(int, input().split())
+relacionamentos = [tuple(map(int, input().split())) for _ in range(M)]
+
+# Contar o número de famílias
+resultado = contar_familias(N, relacionamentos)
+
+# Exibir o resultado
+print(resultado)
